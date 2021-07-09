cd480bb446f1fcf31dd1883a6f0be19692cf7cc2
sethbywater <seth.bywater@gmail.com>
2021-07-09 11:24:14 -0400

    Serves Markdown

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 #![feature(decl_macro)]
 
+mod posts;
+
 use std::{
-    path::{Path, PathBuf},
-    fs::read_to_string
+    path::PathBuf,
+    collections::HashMap,
 };
 use rocket::{
-    response::Responder,
     http::ContentType,
     fs::NamedFile,
     get,
     routes,
 };
-use rocket_contrib::templates::Template;
-use tera::{Context, Tera};
-use serde_json::json;
-// use pulldown_cmark::{html, Parser};
+use tera::{Context, Tera, Function, Value};
 use lazy_static::lazy_static;
+use serde_json::{to_value, from_value};
+
+use crate::posts::Posts;
 
 #[get("/")]
 async fn index() -> (ContentType, String) {
@@ -25,6 +26,14 @@ async fn index() -> (ContentType, String) {
 #[get("/resume")] 
 async fn resume() -> (ContentType, String) {
     (ContentType::HTML, TEMPLATES.render("resume.html", &Context::new()).unwrap())
+}
+
+#[get("/blog/<title>")]
+async fn blog_post(title: String) -> (ContentType, String) {
+    let mut context = Context::new();
+    context.insert("title", &title);
+    context.insert("content", POSTS.get(title).expect("Could not find post"));
+    (ContentType::HTML, TEMPLATES.render("single.html", &context).unwrap())
 }
 
 #[get("/favicon.ico")]
@@ -37,14 +46,35 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(PathBuf::from("content").join(file)).await.ok()
 }
 
+fn get_md() -> impl Function {
+    Box::new(move |args: &HashMap<String, Value>| -> tera::Result<Value> {
+        match args.get("name") {
+            Some(val) => match from_value::<String>(val.clone()) {
+                Ok(v) => to_value(POSTS.get(v).unwrap()).map_err(|_e| tera::Error::template_not_found(".md file not found")),
+                Err(e) => Err(e.into()),
+            },
+            None => Err(tera::Error::call_function("Invalid arguments", "get_md"))
+        }
+    })
+}
+
 lazy_static! { // Allows non-const functions to be called
     static ref TEMPLATES: Tera = match Tera::new("tera/*.html") {
         Ok(mut t) => {
-            t.autoescape_on(vec!["html"]);
+            t.autoescape_on(vec![]);
+            t.register_function("get_md", get_md());
             t
         },
         Err(e)=> {
             println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+
+    static ref POSTS: Posts = match Posts::new("posts/*.md") {
+        Ok(p) => p,
+        Err(e) => {
+            println!("Failed to build posts, error(s): {}", e);
             ::std::process::exit(1);
         }
     };
@@ -55,8 +85,9 @@ async fn main() {
     let _= rocket::build()
         .mount("/", routes![
             index,
-            favicon,
             resume,
+            blog_post,
+            favicon,
             files,
         ])
         .launch().await;
---
+++ src/posts.rs
@@ -0,0 +1,35 @@
+use std::{collections::BTreeMap, path::PathBuf};
+use std::fs::read_to_string;
+use glob::{glob, GlobError};
+use pulldown_cmark::{html, Parser};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Posts (Box<BTreeMap<PathBuf, String>>);
+
+impl Posts {
+    pub fn new(path: &str) -> Result<Self, GlobError> {
+        let mut inner = Box::new(BTreeMap::new());
+        for entry in glob(path).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(title) => {
+                    // The content is moved three times by my count, 
+                    // but it happens at compile time so who cares
+                    println!("Found post: {:?}", title);
+                    let content = read_to_string(&title).expect("Failed to read file");
+                    let parser = Parser::new(&content);
+                    let mut html_buf = String::with_capacity(content.capacity() * 3 / 2);
+                    html::push_html(&mut html_buf, parser);
+
+                    inner.insert(title.strip_prefix("posts/").unwrap().with_extension(""),  html_buf);
+                }
+                Err(e) => return Err(e.into())
+            }
+        }
+        Ok(Posts(inner))
+    }
+
+    pub fn get(&self, k: String) -> Option<&String> {
+        self.0.get(&PathBuf::from(k))
+    }
+}
